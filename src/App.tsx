5965410e4cd29e0722393b89b7e7a93d3ed24690
juvv9 <jun.nakama1030@gmail.com>
2024-03-20 23:02:07 +0900

    ボタンに機能をつける

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from "react";
+
 // import logo from './logo.svg';
 import './App.css';
 
@@ -13,8 +14,15 @@ function App() {
 }
 
 function LikeButton() {
-  const count = 999;
-  return <span className='likeButton'>♥ {count}</span>
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  return  (
+    <span className='likeButton' onClick={handleClick}>
+      ♥ {count}
+    </span>
+  )
 }
 
 export default App;
